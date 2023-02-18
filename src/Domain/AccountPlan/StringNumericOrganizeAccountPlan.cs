36cb05c26d092f4f154b31db1cbe3a504b8a04bb
slipalison <alisonmsn@hotmail.com>
2023-02-18 01:33:36 -0300

    lint

--- src/Domain/AccountPlan/StringNumericOrganizeAccountPlan.cs
+++ src/Domain/AccountPlan/StringNumericOrganizeAccountPlan.cs
@@ -6,24 +6,17 @@ public class StringNumericOrganizeAccountPlan : IComparer<string>
     {
         var xSegments = x.Split('.').Select(int.Parse).ToList();
         var ySegments = y.Split('.').Select(int.Parse).ToList();
-        var i = 0;
 
+        var i = 0;
         while (i < xSegments.Count && i < ySegments.Count)
         {
-            if (xSegments[i] < ySegments[i])
-                return -1;
-
-            if (xSegments[i] > ySegments[i])
-                return 1;
+            if (xSegments[i] < ySegments[i]) return -1;
+            if (xSegments[i] > ySegments[i]) return 1;
             i++;
         }
 
-        if (i < xSegments.Count)
-            return 1;
-
-        if (i < ySegments.Count)
-            return -1;
-
+        if (i < xSegments.Count) return 1;
+        if (i < ySegments.Count) return -1;
         return 0;
     }
 }
